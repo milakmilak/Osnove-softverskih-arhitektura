6652ac279e042772693a883a86a2ddfc7f24666b
Hybrid praksa <hybridpraksa@Hybrids-MacBook-Pro.local>
2021-05-22 18:03:08 +0200

    Initial commit

---
+++ src/main/java/com/osa/osaproject/OsaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.osa.osaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsaProjectApplication.class, args);
+	}
+
+}
